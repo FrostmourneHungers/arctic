0c88dce46f1a697f6ab23c06d0001abaaca079dd
FrostmourneHungers <36342581+FrostmourneHungers@users.noreply.github.com>
2021-08-24 11:02:53 +0200

    Add Python 3.7 support

--- setup.py
+++ setup.py
@@ -112,6 +112,7 @@ setup(
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: Implementation :: CPython",
         "Operating System :: POSIX",
         "Operating System :: MacOS",
